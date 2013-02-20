c55fd81bac4cb31ed46946e48812f53f280d0c1b
Rafael Kitover <rkitover@cpan.org>
2013-02-20 11:15:01 -0500

    initial commit

---
+++ lib/Dist/Zilla/Plugin/Git/PushInitial.pm
@@ -0,0 +1,46 @@
+use utf8;
+package Dist::Zilla::Plugin::Git::PushInitial;
+
+use strict;
+use warnings;
+use 5.008001;
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::Plugin::Git::PushInitial - abstract goes here
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::Git::PushInitial;
+
+=head1 DESCRIPTION
+
+=cut
+
+# declarations go here
+
+=head1 FUNCTIONS/METHODS
+
+=head2 some_function
+
+=cut
+
+# some_function goes here
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<Some::Module>
+
+=back
+
+=head1 AUTHOR
+
+Rafael Kitover <rkitover@cpan.org>
+
+=cut
+
+__PACKAGE__; # End of Dist::Zilla::Plugin::Git::PushInitial
